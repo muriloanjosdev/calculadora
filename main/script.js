9b041865a0f7c03e85ed9786a03e4eef4cc90c3c
muriloanjosdev <murilodosanjos.dev@gmail.com>
2026-03-26 02:59:02 -0300

    commit

--- main/script.js
+++ main/script.js
@@ -2,22 +2,30 @@ let screen = document.getElementById("screen");
 let currentInput = "";
 let currentOperator = "";
 
-function appendNumber(value){
+function appendNumber(value) {
+    if (currentInput.toString().length >= 16) return;
     currentInput = currentInput + value;
     screen.textContent = currentInput;
 }
 
-function appendOperator(operator){
-    if(currentInput === "" && operator !== ".") return;
+function appendOperator(operator) {
+    if (currentInput === "" && operator !== ".") return;
+    if (currentInput.toString().length >= 16) return;
+
+    const lastChar = currentInput.toString().slice(-1);
+    if (["+", "-", "*", "/", "%", "."].includes(lastChar)) {
+        currentInput = currentInput.toString().slice(0, -1);
+    }
+
     currentInput = currentInput + operator;
-    screen.textContent = currentInput; 
+    screen.textContent = currentInput;
 }
 
-function calculate(){
+function calculate() {
     try {
         let result = eval(currentInput);
         if (!Number.isInteger(result)) {
-            result = result.toFixed(3);
+            result = result.toFixed(2);
         }
         currentInput = result;
         screen.textContent = currentInput;
@@ -31,3 +39,9 @@ function clearScreen() {
     currentInput = "";
     screen.textContent = currentInput;
 }
+
+function clearEntry() {
+    currentInput = currentInput.toString().slice(0, -1);
+    screen.textContent = currentInput;
+}
+
