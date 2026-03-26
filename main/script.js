1b6a0397e7ec988fcd3a1bbbe4d910805b8e1ebc
muriloanjosdev <murilodosanjos.dev@gmail.com>
2026-03-26 14:01:58 -0300

    commit

--- main/script.js
+++ main/script.js
@@ -1,6 +1,5 @@
 let screen = document.getElementById("screen");
 let currentInput = "";
-let currentOperator = "";
 
 function appendNumber(value) {
     if (currentInput.toString().length >= 16) return;
@@ -10,7 +9,6 @@ function appendNumber(value) {
 
 function appendOperator(operator) {
     if (currentInput === "" && operator !== ".") return;
-    if (currentInput.toString().length >= 16) return;
 
     const lastChar = currentInput.toString().slice(-1);
     if (["+", "-", "*", "/", "%", "."].includes(lastChar)) {
